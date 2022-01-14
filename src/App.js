8987f5a26c1df25c426a456f76c092181370fd08
Helo88 <israaawad474@gmail.com>
2022-01-14 19:48:15 +0200

    add starters

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="" style={{}}>
+      HELLO WORLD
+      <Router>
+        {/* <NavBar/> */}
+        {/* <Switch>
+            <Route exact component={}  />
+            <Route   exact component={}/>
+            <Route  exact component={}/>
+            <Route path={"*"} exact component={} />
+           
+          </Switch> */}
+        {/* <Footer /> */}
+      </Router>
+    </div>
+  );
+}
+
+export default App;
