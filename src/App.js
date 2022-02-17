f1d660ae8f8554790b1f14221ff331e49896615b
Hanan Mohamed <7ananm7mddd@gmail.com>
2022-02-17 19:04:12 +0200

    show plans and exercises for workouts and yoga

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./style/App.css";
+import Homepage from './components/Homepage';
+import RegisterForm from './components/RegisterForm'
+import ShowPlans from './plans/ShowPlans'
+import Exercises from './plans/exercises'
+import ExDetails from "./plans/ExDetails"
+import ShowYogaPlans from './plans/showYogaPlans'
+import YogaExercises from './plans/YogaExercises'
+import YogaDetails from "./plans/YogaDetails";
+import { useState } from "react";
 
 function App() {
+  const [state, setState] = useState('start')
   return (
     <div className="" style={{}}>
-      HELLO WORLD
+      {/* HELLO WORLD */}
       <Router>
         {/* <NavBar/> */}
-        {/* <Switch>
-            <Route exact component={}  />
-            <Route   exact component={}/>
-            <Route  exact component={}/>
-            <Route path={"*"} exact component={} />
+         <Switch>
+            <Route path={"/"} exact component={ShowPlans}/>
+            <Route path={"/exercises"}  exact component={Exercises}/>
+            <Route path={"/exercises/details"}  exact component={ExDetails}/>
+            <Route path={"/yoga"} exact component={ShowYogaPlans}/>
+            <Route path={"/yogaExercises"}  exact component={YogaExercises}/>
+            <Route path={"/yoga/details"}  exact component={YogaDetails}/>
+            {/* <Route  exact component={}/>
+            <Route path={"*"} exact component={} /> */}
            
-          </Switch> */}
+          </Switch> 
         {/* <Footer /> */}
       </Router>
     </div>
---
+++ src/plans/YogaExercises.js
@@ -0,0 +1,92 @@
+import React, { Component } from "react"
+import "./planStyle.css";
+import myimg from "../yoga2.jpg"
+import { Link } from 'react-router-dom';
+
+class YogaExercises extends Component {
+  constructor(props) {
+    super(props);
+    console.log(this.props.location.state)
+    this.state = {
+      exList: []
+      };
+  }
+
+    async componentDidMount() {
+      try {
+        document.body.style.backgroundColor = "#4FBDBA"
+        document.body.style.minHeight = "130vh"
+        document.body.style.minWidth = "100vw"
+        const res = await fetch('http://localhost:8000/yogaplans/');
+        const exList = await res.json();
+        this.setState({
+          exList
+        });
+      } catch (e) {
+        console.log(e);
+    }
+    }
+  
+      
+    renderItems = () => {
+      
+      const newItems = this.state.exList.filter(
+        item => item.name === this.props.location.state);
+      
+      return newItems.map(item => (
+          
+        <div
+          key={item.id}
+          className="list-group-item d-flex justify-content-between align-items-center"
+        >
+           
+          <span>
+          
+          <div style={{
+               backgroundImage: `url(${myimg})`,
+               backgroundSize:'cover',
+               minWidth: "49vw",
+               minHeight: "50vh", 
+               opacity: 0.9
+          }}> 
+
+          
+          <span className="main2 pt-3">{item.name} Plan</span><br/>
+          <span className="main2 mt-1">Total Duration : {Math.floor(item.totalDuration / 60)}:{item.totalDuration - 
+             (Math.floor(item.totalDuration / 60)) * 60}  minutes</span>
+            
+             </div> 
+            {
+                (item.exercises).map(ex => ( 
+                <div key={ex.id}> 
+                <Link to={{pathname:"/yoga/details", state: ex,}}>
+                <span className="card" id="ex">{ex}</span>
+                </Link>
+                 </div>  ))
+            }
+            
+              
+            </span>
+        </div>
+      ));
+    };
+
+    render() {
+      return (
+        <main>
+        <div className="row">
+        <h1 className="f h1 d-flex justify-content-center mt-5">Exercises</h1>
+          <div className="col-md-6 col-sm-10 mx-auto p-0 mt-6">
+            <div className="card">
+              <ul className="list-group list-group-flush">
+                {this.renderItems()}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </main>
+      )
+    }
+  }
+
+export default YogaExercises;
