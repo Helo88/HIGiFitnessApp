6e1b58f4e9963f08d74641d6991c6660641b81b1
EsraaAldih <esraa.aldih123@gmail.com>
2022-02-21 22:59:27 +0200

    edit water component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -39,25 +39,25 @@ const Navbar = () => {
                 <Link className="nav-link" to={""}> <strong>Shops</strong></Link>
               </li>
 
-              <li class="nav-item dropdown" id="myDropdown">
-                <Link class="nav-link dropdown-toggle" to={""} data-bs-toggle="dropdown"> <strong>Fitness Tracker</strong>  </Link>
-                <ul class="dropdown-menu">
-                  <li> <Link class="dropdown-item" to={""}> Plans </Link>
-                    <ul class="submenu dropdown-menu">
-                      <li><Link class="dropdown-item" to={"/plans"}>Workout</Link></li>
-                      <li><Link class="dropdown-item" to={"/yoga"}>Yoga</Link></li>
+              <li className="nav-item dropdown" id="myDropdown">
+                <Link className="nav-link dropdown-toggle" to={""} data-bs-toggle="dropdown"> <strong>Fitness Tracker</strong>  </Link>
+                <ul className="dropdown-menu">
+                  <li> <Link className="dropdown-item" to={""}> Plans </Link>
+                    <ul className="submenu dropdown-menu">
+                      <li><Link className="dropdown-item" to={"/plans"}>Workout</Link></li>
+                      <li><Link className="dropdown-item" to={"/yoga"}>Yoga</Link></li>
                     </ul>
                   </li>
-                  <li> <Link class="dropdown-item" to={""}> Healthy Life </Link>
-                    <ul class="submenu dropdown-menu">
-                      <li><Link class="dropdown-item" to={""}> Water Tracker</Link></li>
-                      <li><Link class="dropdown-item" to={""}>Weight Tracker</Link></li>
+                  <li> <Link className="dropdown-item" to={""}> Healthy Life </Link>
+                    <ul className="submenu dropdown-menu">
+                      <li><Link className="dropdown-item" to={""}> Water Tracker</Link></li>
+                      <li><Link className="dropdown-item" to={""}>Weight Tracker</Link></li>
                       <hr className="navbar-divider" />
-                      <li><Link class="dropdown-item" to={"/healthytips"}><strong>Healthy Tips</strong></Link></li>
+                      <li><Link className="dropdown-item" to={"/healthytips"}><strong>Healthy Tips</strong></Link></li>
                     </ul>
                   </li>
                   <hr className="navbar-divider" />
-                  <li><Link class="dropdown-item important" to={""}> <strong>Report an issue </strong> </Link></li>
+                  <li><Link className="dropdown-item important" to={""}> <strong>Report an issue </strong> </Link></li>
                 </ul>
               </li>
 
@@ -73,7 +73,7 @@ const Navbar = () => {
 
             <div className="d-grid gap-2 d-md-flex ">
                
-               {token? <Link className="btn btn-light hoverable" to={""}> <strong>Log out</strong></Link> : <Link id="sign-btn" class="btn  hoverable" to={""}> <strong>Sign up</strong></Link> }
+               {token? <Link className="btn btn-light hoverable" to={""}> <strong>Log out</strong></Link> : <Link id="sign-btn" className="btn  hoverable" to={""}> <strong>Sign up</strong></Link> }
                
                {!token? <Link className="btn btn-light hoverable" to={"/login"}> <strong>Log In</strong></Link>:<span></span> }
                
--- src/components/Water.js
+++ src/components/Water.js
@@ -3,14 +3,15 @@ import { useState, useEffect } from "react";
 import Swal from 'sweetalert2'
 // import withReactContent from 'sweetalert2-react-content'
 import axios from "axios";
-import {axiosInstance} from "../js/network/index";
+import { axiosInstance } from "../js/network/index";
 
 
 const Water = () => {
-    const [count, setCount] = useState({currentAmount :0});
+    const [count, setCount] = useState({ currentAmount: 0 });
+    const [totalAmount, setAmount] = useState(0);
     var d = new Date()
     let date = d.getHours() + ":" + d.getMinutes();
-
+    const waterAmount = 3000
     const swalWithBootstrapButtons = Swal.mixin({
         customClass: {
             confirmButton: 'btn btn-success',
@@ -21,68 +22,88 @@ const Water = () => {
 
     useEffect(() => {
         // const interval = setInterval(() => {
-            axiosInstance.get('http://127.0.0.1:8000/water/',{				
-            })    
-        // axios.get(`http://127.0.0.1:8000/water/${localStorage.getItem('id')}/`, {
-        //     headers:headers
-        //   })
-        .then((res) => {
-            console.log( res)
-            setCount({currentAmount :res.data.currentAmount})
-            return res.data
-        }).then((data)=>{
-            console.log(count)
-
+        axiosInstance.get('http://127.0.0.1:8000/water/', {
         })
-        .then((res)=>{
-            if (date === "3:30" || date === "3:31" || date === "2:53"  || date === "18:00"  || date === "21:00"){
-                console.log("nnnnnnnnnnnnnnnnnnnnnnn")
-                swalWithBootstrapButtons.fire({
-                    title: 'Did you drink water ?',
-                    text: "Confirm or Dismiss!",
-                    icon: 'warning',
-                    showCancelButton: true,
-                    confirmButtonText: 'Yes,I did it!',
-                    cancelButtonText: 'No, cancel!',
-                    reverseButtons: true
-                }).then((result) => {
-                    if (result.isConfirmed) {
-                        swalWithBootstrapButtons.fire(
-                            'Good Job!',
-                            'success'
-                        )
-                        setCount(count.currentAmount = count.currentAmount+1)
-                        console.log(count.currentAmount)
 
-                        axiosInstance.put(`http://127.0.0.1:8000/water/`,{'count':count.currentAmount},{
+            .then((res) => {
+                console.log(res)
+                return res.data
+            }).then((data) => {
 
-                        })
-    
-                    } else if (
-                        /* Read more about handling dismissals below */
-                        result.dismiss === Swal.DismissReason.cancel
-                    ) {
-                        swalWithBootstrapButtons.fire(
-                            'Cancelled',
-                            'You have to drink a cup water :)',
-                            'error'
-                        )
+                Swal.fire({
+                    title: `Your current amount of water ${data.result} Mliter from daily amount 3000 Mliter`,
+                    showClass: {
+                        popup: 'animate__animated animate__fadeInDown'
+                    },
+                    hideClass: {
+                        popup: 'animate__animated animate__fadeOutUp'
                     }
                 })
-    
-                console.log("false")
-            }else{
-                console.log("true")
-                console.log(date)
-                console.log(count)
-            }
-    
-        })
 
-//     }, 60000);
+                return data;
+            })
+            .then((data) => {
+                setCount({ currentAmount: data.result })
+                console.log(count.currentAmount)
+                if (date === "22:21" || date === "21:21" || date === "21:48" || date === "9:0" || date === "12:0" || date === "15:0" || date === "18:0" || date === "21:0" || date === "0:0") {
+                    console.log("nnnnnnnnnnnnnnnnnnnnnnn")
+                    if (data.result < 3000) {
 
-//  return () => clearInterval(interval);
-    }, [])
+                        swalWithBootstrapButtons.fire({
+                            title: 'Did you drink water ?',
+                            text: "Confirm or Dismiss!",
+                            icon: 'warning',
+                            showCancelButton: true,
+                            confirmButtonText: 'Yes,I did it!',
+                            cancelButtonText: 'No, cancel!',
+                            reverseButtons: true
+                        }).then((result) => {
+                            if (result.isConfirmed) {
+                                swalWithBootstrapButtons.fire(
+                                    'Good Job!',
+                                    'success'
+                                )
+        
+                                    axiosInstance.put(`http://127.0.0.1:8000/water/`, { 'count': (data.result) + 500 }, {
+                                    })
+                                
+
+
+                            } else if (result.dismiss === Swal.DismissReason.cancel) {
+                                swalWithBootstrapButtons.fire(
+                                    'Cancelled',
+                                    'You have to drink some water :)',
+                                    'error'
+                                )
+                            }
+
+                        })
+                    }
+                    else {
+                            Swal.fire({
+                                title: 'Good Job , Keep in going, You have reached the goal',
+                                showClass: {
+                                    popup: 'animate__animated animate__fadeInDown'
+                                },
+                                hideClass: {
+                                    popup: 'animate__animated animate__fadeOutUp'
+                                }
+                            })
+                        } 
+
+                    console.log("true")
+                } else {
+                    console.log("false")
+                    console.log(date)
+                    console.log(count)
+                }
+
+            })
+
+        //     }, 60000);
+
+        //  return () => clearInterval(interval);
+    }, [d.getMinutes()])
     return (
         <div>
             {/* {count} */}
