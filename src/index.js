1ede208112028ef058b4d63e796deed90ecfb223
halahamde <36208722+halahamde@users.noreply.github.com>
2022-02-23 04:39:29 +0200

    Edit weight tracker and add trainee profile-Hala (#18)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { CookiesProvider } from "react-cookie";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/js/bootstrap.min.js";
@@ -10,6 +9,5 @@ import "./style/index.css";
 import "leaflet/dist/leaflet.css"
 
 ReactDOM.render( 
-<CookiesProvider>
     <App />
- </CookiesProvider>, document.getElementById("root"));
+, document.getElementById("root"));
