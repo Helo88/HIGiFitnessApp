118d115861622236e716a2420cbf553a20a9bf73
EsraaAldih <47404576+EsraaAldih@users.noreply.github.com>
2022-02-27 02:36:10 +0200

    Login form:Esraa (#24)
    * login page
    
    * login page html

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,8 +7,9 @@ import Visibility from "@material-ui/icons/Visibility";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import VisibilityOff from "@material-ui/icons/VisibilityOff";
 import Input from "@material-ui/core/Input";
-
-export default function SignIn() {
+import img from "../images/wory.jpg"
+import "../style/login.css"
+const SignIn = () => {
   const history = useHistory();
   const [trainerDetail, setTrainerDetail] = useState({});
 
@@ -107,7 +108,7 @@ export default function SignIn() {
               localStorage.setItem("address", res.fields.address);
               console.log("my details ", trainerDetail);
             })
-            .then(history.push("/check"));
+            .then(history.push("/"));
         } else {
           axiosInstance
             .get(`http://127.0.0.1:8000/users/traineeDetail/`, {
@@ -127,7 +128,7 @@ export default function SignIn() {
               localStorage.setItem("workoutPlan", res.fields.workoutPlan);
               localStorage.setItem("yogaPlan", res.fields.yogaPlan);
             })
-            .then(history.push("/me"));
+            .then(history.push("/"));
         }
       })
 
@@ -137,62 +138,90 @@ export default function SignIn() {
       });
   };
 
-  return (
-    <Container component="main" maxWidth="xs">
-      <form
-        style={{ minWidth: "450px", maxWidth: "500px" }}
-        onSubmit={(e) => handleSubmit(e)}
-      >
-        <div className="mb-3">
-          <h2 className="h2 mb-5">Register Now!</h2>
-          <label htmlFor="exampleInputName" className="form-label h3">
-            Name
-          </label>
-          <div className="mb-3">
-            <label htmlFor="exampleInputEmail1" className="form-label h3">
-              Email Address
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Please Enter your email"
-              name="email"
-              value={userForm.email}
-              onChange={(e) => handleChange(e)}
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
+    return (
+        <div className="content">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6 order-md-2 ">
+              <img src={img} alt="Image" className="img-fluid"/>
+            </div>
+            <div className="col-md-6 contents">
+              <div className="row justify-content-center">
+                <div className="col-md-8 ">
+                  <div className="mb-4">
+                  <h2>Sign In to <strong>H<span className="hige">i</span>G<span className="hige">E</span></strong></h2>
+                </div>
+                <form onSubmit={(e) => handleSubmit(e)} className="pt-5 ">
+                  <div className="form-group first my-6">
+                    <Input
+                      type="email"
+                      className="form-control"
+                      placeholder="Please Enter your email"
+                      name="email"
+                      value={userForm.email}
+                      onChange={(e) => handleChange(e)}
+                      id="exampleInputEmail1"
+                      aria-describedby="emailHelp"
+                    />    
+                  </div>
+                  <div className="form-group last mb-6">
+                    <label for="password">Password</label>
+                    <Input
+                      type={userForm.showPassword ? "text" : "password"}
+                      onChange={handlePasswordChange("password")}
+                      value={userForm.password}
+                      name="password"
+                      className="form-control"
+                      placeholder="password"
+                      onChange={(e) => handleChange(e)}
+                      id="exampleInputPassword"
+                      endAdornment={
+                        <InputAdornment position="end">
+                          <IconButton
+                            onClick={handleClickShowPassword}
+                            onMouseDown={handleMouseDownPassword}
+                          >
+                            {userForm.showPassword ? <Visibility /> : <VisibilityOff />}
+                          </IconButton>
+                        </InputAdornment>
+                      }
             />
+                    
+                  </div>
+                  {/*                   
+                  <div className="d-flex mb-5 align-items-center">
+                    <label className="control control--checkbox mb-0"><span className="caption">Remember me</span>
+                      <input type="checkbox" checked="checked"/>
+                      <div className="control__indicator"></div>
+                    </label>
+                    <span className="ml-auto"><a href="#" className="forgot-pass">Forgot Password</a></span> 
+                  </div> */}
+    
+                  <input type="submit" value="Log In" className="btn text-white btn-block login"/>
+    
+                  {/* <span className="d-block text-left my-4 text-muted"> or sign in with</span>
+                  
+                  <div className="social-login">
+                    <a href="#" className="facebook">
+                      <span className="icon-facebook mr-3"></span> 
+                    </a>
+                    <a href="#" className="twitter">
+                      <span className="icon-twitter mr-3"></span> 
+                    </a>
+                    <a href="#" className="google">
+                      <span className="icon-google mr-3"></span> 
+                    </a>
+                  </div> */}
+                </form>
+                </div>
+              </div>
+              
+            </div>
+            
           </div>
-          <div className="mb-3">
-            <label htmlFor="exampleInputPassword" className="form-label h3">
-              Password
-            </label>
-            <Input
-              type={userForm.showPassword ? "text" : "password"}
-              onChange={handlePasswordChange("password")}
-              value={userForm.password}
-              name="password"
-              className="form-control"
-              placeholder="password"
-              onChange={(e) => handleChange(e)}
-              id="exampleInputPassword"
-              endAdornment={
-                <InputAdornment position="end">
-                  <IconButton
-                    onClick={handleClickShowPassword}
-                    onMouseDown={handleMouseDownPassword}
-                  >
-                    {userForm.showPassword ? <Visibility /> : <VisibilityOff />}
-                  </IconButton>
-                </InputAdornment>
-              }
-            />
-          </div>
-          <button type="submit" className="btn">
-            Register
-          </button>
         </div>
-      </form>
-    </Container>
-  );
+      </div>
+    );
 }
+
+export default SignIn;
